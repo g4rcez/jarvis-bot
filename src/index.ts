191ef4837e7bfc5c39049f81e481951e878fb5bc
Garcez <allan.f.garcez@gmail.com>
2019-01-25 01:26:45 -0200

    THE MAIN

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Message } from "node-telegram-bot-api";
+import { Command, Commands } from "./commands";
+import { config } from "./config";
+
+const { bot } = config;
+
+Commands.forEach((x: Command) => {
+    if (x.middlewares) {
+        return;
+    }
+    bot[x.event](x.pattern, x.action);
+});
+
+// Grep all the things to make a little zueira
+bot.on("message", (msg: Message) => {
+    const chatId = msg.chat.id;
+    if (`${msg.text}`.match(/.*python.*/gi)) {
+        bot.sendMessage(chatId, "Não fala Python, seu cuzão");
+    }
+});
